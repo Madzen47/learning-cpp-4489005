1f9fe0f1224c4a3d51406b25a8cf8107549ac461
Madzen47 <122415596+Madzen47@users.noreply.github.com>
2024-07-24 20:48:14 +0000

    exercises I missed

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Hi, what's your name? " << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Good moorning, " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
